0c8e94dc8d3287c502a0f78877b9a2ae18d198d3
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-05-19 23:44:31 +0100

    12. Alterando e destruindo

--- main.tf
+++ main.tf
@@ -1,25 +1,27 @@
 terraform {
-  required_version = "0.14.4"
+  required_version = ">= 0.14.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.23.0"
+      version = "~> 6.28.0"
     }
   }
 }
 
 
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-123wss"
+  bucket = "my-tf-test-bucket-123wssoutros"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My first bucket"
     Environment = "Dev"
     ManagedBy   = "Terraform"
+    Owner       = "Walyson Silva"
+    Updated     = "2026-05-18"
   }
 }
